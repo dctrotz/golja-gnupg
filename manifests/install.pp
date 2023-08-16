6fa49cc316fac7c56552d2a9c26431397d615c44
David Trotz <davidtrotz@olivetree.com>
2023-08-16 13:53:14 -0700

    Fixes issue with other modules also declaring package gnupg
    The proper way to approach this is with `ensure_packages` as long as others play nice this will work, if they don't, well neither this nor the way it is now works.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,9 +1,4 @@
 #
 class gnupg::install {
-
-  package { 'gnupg':
-    ensure => $gnupg::package_ensure,
-    name   => $gnupg::package_name,
-  }
-
+  ensure_packages(['gnupg'], { ensure => $gnupg::package_ensure })
 }
